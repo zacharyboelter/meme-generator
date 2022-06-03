a3517773004ce4729b30a3b5008acfa469356474
Zachary Boelter <zboelter@gmail.com>
2022-06-03 12:33:14 -0700

    classname for div

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from "./components/Header"
  */
 export default function App() {
     return (
-        <Header />
+        <div className="main-container">
+            <Header />
+        </div>
     )
 }
