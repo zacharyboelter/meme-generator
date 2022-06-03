38c3d5fd8fb0710f4f322c459027752bf76227e4
Zachary Boelter <zboelter@gmail.com>
2022-06-03 12:40:20 -0700

    styles linked and initial styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function Header() {
     return (
-        
-        <h1>hello world</h1>
-        
+        <header>
+            <img src="../images/troll-face.png" alt="" className="troll-face"/>
+            <h1>Meme Generator</h1>
+
+        </header>
+
     )
 }
