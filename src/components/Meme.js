83f6efb405b650045f6f6eb06f8e08f4f11875e3
Zachary Boelter <zboelter@gmail.com>
2022-06-06 13:38:34 -0700

    form styling

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,8 +4,16 @@ export default function Meme() {
     return (
         <main>
             <form action="" className="form--container">
-                <input type="text" className="form--input" placeholder="Shut up" />
-                <input type="text" className="form--input" placeholder="and take my money" />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="Shut up" 
+                />
+                <input 
+                    type="text" 
+                    className="form--input" 
+                    placeholder="and take my money" 
+                />
                 <button className="form--btn">Get new meme image 🖼</button>
             </form>
         </main>
