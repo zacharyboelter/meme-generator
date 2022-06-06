d16b014f304c501014add14f9cb9d82a130ea02c
Zachary Boelter <zboelter@gmail.com>
2022-06-06 13:29:34 -0700

    intitial form

---
+++ src/components/Meme.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form action="" className="form--container">
+                <input type="text" className="form--input" placeholder="Shut up" />
+                <input type="text" className="form--input" placeholder="and take my money" />
+                <button className="form--btn">Get new meme image 🖼</button>
+            </form>
+        </main>
+    )
+}
