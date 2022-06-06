37ba55e555a688bcf1ecbef8612810bec99e90c4
Zachary Boelter <zboelter@gmail.com>
2022-06-06 14:08:16 -0700

    new getMemeFucntion beginnings

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,21 +1,31 @@
 import React from "react";
 
 export default function Meme() {
+    function getMemeImage(){
+        
+    }
+
     return (
         <main>
-            <form action="" className="form--container">
-                <input 
-                    type="text" 
-                    className="form--input" 
-                    placeholder="Shut up" 
+            <div action="" className="form--container">
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="Shut up"
                 />
-                <input 
-                    type="text" 
-                    className="form--input" 
-                    placeholder="and take my money" 
+                <input
+                    type="text"
+                    className="form--input"
+                    placeholder="and take my money"
                 />
-                <button className="form--btn">Get new meme image 🖼</button>
-            </form>
+                <button
+                    className="form--btn"
+                    onClick={getMemeImage}
+                    >
+                    Get new meme image 🖼
+
+                </button>
+            </div>
         </main>
     )
 }
