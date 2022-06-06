51839fe3a1095facd81ac36ee491042d6e0aa647
Zachary Boelter <zboelter@gmail.com>
2022-06-06 13:17:21 -0700

    header complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,10 @@ import React from "react";
 
 export default function Header() {
     return (
-        <header>
-            <img src="../images/troll-face.png" alt="" className="troll-face"/>
-            <h1>Meme Generator</h1>
-
+        <header className="header">
+            <img src="../images/troll-face.png" alt="" className="troll-face" />
+            <h2 className="header-title">Meme Generator</h2>
+            <p className="header-sub">React Course - Project 3</p>
         </header>
 
     )
