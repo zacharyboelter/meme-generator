0e95097e7d7fe854acdce9fb31aa018d02a3175a
Zachary Boelter <zboelter@gmail.com>
2022-06-07 15:35:56 -0700

    initialize database with mockup

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState('')
+
+
+
     function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
         
     }
 
@@ -27,5 +34,41 @@ export default function Meme() {
                 </button>
             </div>
         </main>
+    )
+}
+
+
+
+
+
+export default function App() {
+    const [count, setCount] = React.useState(0)
+    /**
+     * Note: if you ever need the old value of state
+     * to help you determine the new value of state,
+     * you should pass a callback function to your
+     * state setter function instead of using
+     * state directly. This callback function will
+     * receive the old value of state as its parameter,
+     * which you can then use to determine your new
+     * value of state.
+     */
+    function add() {
+        setCount(prevCount => prevCount + 1)
+    }
+    // Challenge: update `substract` to use a callback function
+    
+    function subtract() {
+        setCount(prevCount => prevCount - 1)
+    }
+    
+    return (
+        <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
     )
 }
