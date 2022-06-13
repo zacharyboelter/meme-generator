b0371dc5b5b3aa5dd466e2b7955e8485cf8fcc0b
Zachary Boelter <zboelter@gmail.com>
2022-06-13 13:03:37 -0700

    useState with object instead of array

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -114,3 +114,49 @@ export default function Meme() {
 //         email: "itsmyrealname@example.com",
 //         isFavorite: false
 //     })
+
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (719) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: true
+    })
+    /**
+     * Challenge: Use a ternary to determine which star image filename
+     * should be used based on the `contact.isFavorite` property
+     * 
+     * `true` => "star-filled.png"
+     * `false` => "star-empty.png"
+     */
+    
+//     let starIcon = contact.isFavorite ? "star-filled.png" : "star-empty.png"
+    
+    
+//     function toggleFavorite() {
+//         console.log("Toggle Favorite")
+//     }
+    
+//     return (
+//         <main>
+//             <article className="card">
+//                 <img src="./images/user.png" className="card--image" />
+//                 <div className="card--info">
+//                     <img 
+//                         src={`../images/${starIcon}`} 
+//                         className="card--favorite"
+//                         onClick={toggleFavorite}
+//                     />
+//                     <h2 className="card--name">
+//                         {contact.firstName} {contact.lastName}
+//                     </h2>
+//                     <p className="card--contact">{contact.phone}</p>
+//                     <p className="card--contact">{contact.email}</p>
+//                 </div>
+                
+//             </article>
+//         </main>
+//     )
+// }
