4de1e9c96c881a5bd80c54373a6583438fcb9add
Zachary Boelter <zboelter@gmail.com>
2022-06-21 13:48:12 -0700

    forms. set onChange to hangleChange. Function that updates state with every click

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -239,3 +239,25 @@ export default function App() {
 //         </div>
 //     )
 // }
+
+// import React from "react"
+
+// export default function Form() {
+//     const [firstName, setFirstName] = React.useState("")
+    
+//     console.log(firstName)
+    
+//     function handleChange(event) {
+//         setFirstName(event.target.value)
+//     }
+    
+//     return (
+//         <form>
+//             <input
+//                 type="text"
+//                 placeholder="First Name"
+//                 onChange={handleChange}
+//             />
+//         </form>
+//     )
+// }
